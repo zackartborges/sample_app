d0387091f9f309a4f7502b7176165cf5b56663e5
Zack Borges <zackartborges@gmail.com>
2021-02-22 15:06:08 -0500

    adds 3 new paths (dont completley understand how yet)

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,2 +1,13 @@
 class Api::ExamplePagesController < ApplicationController
+  def hello_action
+    render json: { message: "hello" }
+  end
+
+  def hello_there
+    render json: { message: "what is up?" }
+  end
+
+  def goodness
+    render json: { message: "goodnight!" }
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # get "/photos" => "photos#index"
 
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
-  # namespace :api do
-  #   get "/photos" => "photos#index"
-  # end
+  namespace :api do
+    get "/hello_path" => "example_pages#hello_action"
+    get "/goodbye_path" => "example_pages#hello_there"
+    get "/yolo" => "example_pages#goodness"
+  end
 end
